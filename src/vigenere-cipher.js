4656917fddcfb04951179f384262df1bf47d5412
Yuliya Runova <jul.runova@gmail.com>
2022-05-02 13:12:37 +0400

    feat: new tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+function isRequired() {
+  throw Error('Incorrect arguments!');
+};
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _startCode = 'A'.charCodeAt(0);
+  _base = 26;
+
+  constructor (isDirect=true) {
+    this._isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  _getKey(upperMessage, key) {
+    let len = upperMessage.split("").filter(el => {
+      return (el.charCodeAt(0) < this._startCode + this._base) 
+        && (el.charCodeAt(0) >= this._startCode)
+    }).length;
+    let upperKey = "";
+
+    for(let m = 0; m < Math.ceil(len / key.length); m++) {
+      upperKey += key.toUpperCase();
+    }
+    return upperKey;
+  }
+
+  _algorithm(message, key, sign) {
+    let upperMessage = this._isDirect ? message.toUpperCase() : message.split('').reverse().join('').toUpperCase();
+    let upperKey = this._getKey(upperMessage, key);
+    var encryptedMessage = [];
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let symb = upperMessage.charCodeAt(i);
+      if ((symb <= this._startCode + this._base) 
+        && (symb >= this._startCode)) {
+            encryptedMessage.push(this._startCode 
+              + ((symb
+                  + (this._base + sign * upperKey.charCodeAt(j)))
+                % this._base));
+            j++;
+          }
+      else 
+        encryptedMessage.push(upperMessage.charCodeAt(i));
+    }
+    return String.fromCharCode(...encryptedMessage);    
+  }
+
+
+  encrypt(message=isRequired(), key=isRequired()) {
+    return this._algorithm(message, key, 1)
+  }
+  decrypt(encryptedMessage=isRequired(), key=isRequired()) {
+    return this._algorithm(encryptedMessage, key, -1)
   }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  
+  if (! date) {
+    return 'Unable to determine the time of year!';
+  }  
+  try {
+    if (! (
+      date instanceof Date 
+      && !isNaN(date) 
+      && Object.prototype.toString.call(date) === '[object Date]'
+      && date.valueOf() == date[Symbol.toPrimitive]('number')
+      )) {
+        throw new Error('Invalid date!');
+    }
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
+  
+  let seasons = ['winter', 'winter'
+  , 'spring', 'spring', 'spring'
+  , 'summer', 'summer', 'summer'
+  , 'autumn', 'autumn', 'autumn'
+  , 'winter'];
+  let mth = date.getMonth() == undefined ? -1 : date.getMonth() ;
+  console.log(mth);
+  console.log(seasons[mth]);
+  return seasons[mth] || 'Unable to determine the time of year!';
 }
 
 module.exports = {
